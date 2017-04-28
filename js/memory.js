293f81fd8c06da5263f4c14c97872ca8b4823eb7
hyperdestru <slotuser@gmail.com>
2017-04-28 17:16:43 +0200

    Mise en fonctions (permutation, affichage images), maintenabilité generale

--- js/memory.js
+++ js/memory.js
@@ -5,7 +5,6 @@ var images = [
   "hack",
   "iceland",
   "kix",
-  "mtv",
   "pines",
   "root",
   "tarantino",
@@ -14,13 +13,14 @@ var images = [
   "neptune",
   "wargames",
   "will",
+  "buffy",
+  "bus",
   "bear",
   "cat",
   "dbz",
   "hack",
   "iceland",
   "kix",
-  "mtv",
   "pines",
   "root",
   "tarantino",
@@ -28,61 +28,72 @@ var images = [
   "tron",
   "neptune",
   "wargames",
-  "will"
+  "will",
+  "buffy",
+  "bus"
 ];
 
-// Recupere la source de l'image et donc permet de l'afficher
+// Recupere la source de l'image et donc permetra de l'afficher
 function getSource(nom) {
   var source = "../images/classic/" + nom + ".jpg";
   return source;
 }
 
-var randomImages = images.slice();
-
-var count = 0;
-var tryOut = 0;
-var guess1 = "";
-var guess2 = "";
-// Array qui contiendra les 'caches' img (= div)
-var rCache = [];
-
-// Permutation des images :
-for (var i = 0; i < randomImages.length; i++) {
+function getRandomPermutation(array) {
   // Declare un nouvel index random :
-  var iRandom = i + Math.floor(Math.random() * (randomImages.length - i));
-  var tmp = randomImages[i];
-  randomImages[i] = randomImages[iRandom];
-  randomImages[iRandom] = tmp;
+  var iRandom = i + Math.floor(Math.random() * (array.length - i));
+  var tmp = array[i];
+  array[i] = array[iRandom];
+  array[iRandom] = tmp;
+  return array;
+}
 
+function displayImages(array) {
   var imgElt = document.createElement("img");
   var cacheElt = document.createElement("div");
-  imgElt.src = getSource(randomImages[i]);
+  imgElt.src = getSource(array[i]);
   cacheElt.appendChild(imgElt);
+  // Met les 'caches' div dans l'array rCache pour pouvoir les parcourir et target leurs child (image)
   rCache.push(cacheElt);
   document.getElementById("board").appendChild(cacheElt);
 }
 
+var count = 0;
+var moves = 0;
+var guess1 = "";
+var guess2 = "";
+var rCache = [];
+
+//var randomImages = images.slice();
+
+for (var i = 0; i < images.length; i++) {
+  getRandomPermutation(images);
+  displayImages(images);
+}
+
 // Parcours l'array des 'caches'
 rCache.forEach(function(cache) {
-  // De base les images sont cachées et possedent la classe 'faceDown'
+  // Par defaut les images sont cachées et possedent la classe 'faceDown'
   cache.childNodes[0].style.visibility = "hidden";
   cache.childNodes[0].setAttribute("class", "faceDown");
 
-  // A chaque clic sur un element l'image s'affiche et gagne la classe 'faceUp'
+  // A chaque clic sur un element l'image s'affiche et gagne la classe 'faceUp' :
   cache.addEventListener("click", function(event) {
     event.target.childNodes[0].style.visibility = "visible";
     event.target.childNodes[0].setAttribute("class", "faceUp");
     // Compte le nombre de coups (2 max)
     count++;
 
-    // 1er coup
+    // 1st coup
     if (count === 1) {
       guess1 = event.target.childNodes[0];
-      // 2eme coup, tout se passe a ce niveau, il est decisif
+      // 2nd coup decisif : determine l'etat gagnant (match) ou perdant (boucle sur les div + faceDown)
     } else if (count === 2) {
       guess2 = event.target.childNodes[0];
+      // "Round" fini, reset compteur
       count = 0;
-      tryOut++;
+      // Comptage des coups (position a revoir?)
+      moves++;
 
       // Si il y a un match, chaque image gagne une classe 'match'
       if (guess1.src === guess2.src) {
@@ -91,7 +102,7 @@ rCache.forEach(function(cache) {
         guess2.style.visibility = "visible";
         guess2.setAttribute("class", "match");
       } else {
-        // Retourne toutes les cartes au bout de 1s par defaut sauf les images avec une classe 'match'
+        // Retourne toutes les cartes au bout de 1s par defaut sauf les images avec une classe 'match' (ne fait rien)
         setTimeout(function() {
           for (var i = 0; i < rCache.length; i++) {
             if (rCache[i].childNodes[0].classList.contains("match")) {
@@ -101,7 +112,6 @@ rCache.forEach(function(cache) {
               rCache[i].childNodes[0].setAttribute("class", "faceDown");
             }
           }
-          count = 0;
         }, 1000);
       }
     }
