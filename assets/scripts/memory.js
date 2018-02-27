c02bd5948af671d388e5e0a0ba0260cd70de4e32
hyperdestru <slotuser@gmail.com>
2018-02-27 15:13:16 +0100

    New arch -> homepage w/ own css, theme pages w/ 1 css, modif js based on weither you are on classic page or disney page (different pictures)

--- assets/scripts/memory.js
+++ assets/scripts/memory.js
@@ -1,9 +1,15 @@
 var board = document.getElementById("board");
-var cd = board.children;
+var divs = board.children;
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
   "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"
 ];
 
-for (var i = 0; i <= alphabet.length; i++) {
-  cd[i].firstChild.setAttribute("src", "assets/images/classic/" + alphabet[i] + ".jpg");
+if (!document.getElementById("disney-theme")) {
+  for (var i = 0; i <= alphabet.length; i++) {
+    divs[i].firstChild.setAttribute("src", "assets/images/classic/" + alphabet[i] + ".jpg");
+  }
+} else {
+  for (var i = 0; i <= alphabet.length; i++) {
+    divs[i].firstChild.setAttribute("src", "assets/images/disney/" + alphabet[i] + ".jpg");
+  }
 }
