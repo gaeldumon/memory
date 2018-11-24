dcfded86d8bc3308a7d92e5cf1a813bfcc8a5672
hyperdestru <slotuser@gmail.com>
2018-11-24 17:45:06 +0100

    Complete refactoring of js, functionized it af : shuffle array func, furtive msg func, return cards func...

--- assets/scripts/memory.js
+++ assets/scripts/memory.js
@@ -1,24 +1,54 @@
-var randomPermutation = function (array) {
-  let iRandom = i + Math.floor(Math.random() * (array.length - i));
-  let tmp = array[i];
-  array[i] = array[iRandom];
-  array[iRandom] = tmp;
+/*Shuffles indexes of an array given as a parameter, returns this array shuffled*/
+function shuffleArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    let iRandom = i + Math.floor(Math.random() * (array.length - i));
+    let tmp = array[i];
+    array[i] = array[iRandom];
+    array[iRandom] = tmp;
+  }
   return array;
 }
 
+/*Creates text, links it to a class name in the html or css files, appends it to a parent element & makes it disapear in x sec*/
+function createFurtiveMessage(cssClass, text, parent, time) {
+  var message = document.createElement("p");
+  message.setAttribute("class", cssClass);
+  message.textContent = text;
+  parent.appendChild(message);
+  setInterval(function () {
+    message.style.display = "none";
+  }, time);
+}
+
+function hideElements(array, elementList, classToOverride, newClass, delay) {
+  setTimeout(function () {
+    for (let i = 0; i <= array.length; i++) {
+      if (!elementList[i].firstChild.classList.contains(classToOverride)) {
+        elementList[i].firstChild.style.visibility = "hidden";
+        elementList[i].firstChild.setAttribute("class", newClass);
+      }
+    }
+  }, delay);
+}
+
+/*html elements*/
 var board = document.getElementById("board");
-var divs = board.children;
-var alphabet = "abcdefghijklmnopabcdefghijklmnop".split("");
-var theme = "";
-var matchCount = 0;
-var moveCount = 0;
-var matchCounter = document.getElementsByClassName("matchCounter")[0];
-var moveCounter = document.getElementsByClassName("moveCounter")[0];
-var highScoresLog = document.getElementsByClassName("highScoresLog")[0];
-var guessCount = 0;
+var body = document.getElementsByTagName("body")[0];
+var text_matches = document.getElementsByClassName("matches")[0];
+var text_moves = document.getElementsByClassName("moves")[0];
+var text_highScore = document.getElementsByClassName("highScore")[0];
+var cards_down = board.children;
+
+var cards_names = "abcdefghijklmnopabcdefghijklmnop".split("");
+var theme;
 var guess1;
 var guess2;
-var cheatCount = false;
+var count_matches = 0;
+var count_moves = 0;
+var count_guesses = 0;
+
+localStorage.setItem('s', count_moves);
+text_highScore.textContent = localStorage.getItem('s');
 
 if (document.getElementById("disney-theme")) {
   theme = "disney";
@@ -26,56 +56,41 @@ if (document.getElementById("disney-theme")) {
   theme = "classic";
 }
 
-for (var i = 0; i < alphabet.length; i++) {
-  divs[i].firstChild.setAttribute("src", "assets/images/" + theme + "/" + randomPermutation(alphabet)[i] + ".jpg");
-  divs[i].firstChild.style.visibility = "hidden";
+shuffleArray(cards_names);
 
-  divs[i].addEventListener("click", function (event) {
+for (var i = 0; i < cards_names.length; i++) {
+  cards_down[i].firstChild.setAttribute("src", "assets/images/" + theme + "/" + cards_names[i] + ".jpg");
+  cards_down[i].firstChild.style.visibility = "hidden";
+
+  cards_down[i].addEventListener("click", function (event) {
     event.target.firstChild.style.visibility = "visible";
     event.target.firstChild.setAttribute("class", "cardUp");
-    guessCount += 1;
+    count_guesses += 1;
 
-    if (guessCount === 1) {
+    if (count_guesses === 1) {
       guess1 = event.target.firstChild;
-    } else if (guessCount === 2) {
+    } else if (count_guesses === 2) {
       guess2 = event.target.firstChild;
-      guessCount = 0;
-      moveCount += 1;
-      moveCounter.textContent = "MOVES : " + moveCount;
+      count_guesses = 0;
+      count_moves += 1;
+      text_moves.textContent = "MOVES : " + count_moves;
 
       if (guess1.src === guess2.src) {
         guess1.setAttribute("class", "match");
         guess2.setAttribute("class", "match");
-        matchCount += 1;
-        matchCounter.textContent = "MATCH : " + matchCount;
+        count_matches = 16;
+        text_matches.textContent = "MATCH : " + count_matches;
 
-        /*Creating a match message on the go when there's an actual match*/
-        var matchMsg = document.createElement("p");
-        matchMsg.setAttribute("class", "matchMsg");
-        matchMsg.textContent = "IT'S A MATCH !";
-        board.appendChild(matchMsg);
-
-        //Match message disparears in 1.2s
-        setInterval(function () {
-          matchMsg.style.display = "none";
-        }, 1200);
+        createFurtiveMessage("matchMsg", "IT'S A MATCH", board, 1200);
 
         /*Win state => 16 matchs -> end of the game*/
-        if (matchCount === 16) {
-          matchCounter.textContent = "Tu gagnes en " + moveCount + " coups !";
-          localStorage.setItem('score', moveCount);
-          highScoresLog.textContent = localStorage.getItem('score');
+        if (count_matches === 16) {
+          localStorage.setItem('s', count_moves);
+          text_highScore.textContent = localStorage.getItem('s');
         }
-        /*Lose state => guess 1 & 2 are no match (different cards aka different src) -> all cards w/o match class are switched off*/
+        /*Lose state => guess 1 & 2 are no match (different cards different src) -> all cards w/o match class are switched off*/
       } else {
-        setTimeout(function () {
-          for (var j = 0; j <= alphabet.length; j++) {
-            if (!divs[j].firstChild.classList.contains("match")) {
-              divs[j].firstChild.style.visibility = "hidden";
-              divs[j].firstChild.setAttribute("class", "cardDown");
-            }
-          }
-        }, 900);
+        hideElements(cards_names, cards_down, "match", "cardDown", 900);
       }
     }
   }, false);
